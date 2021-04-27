02d88d5627f49dafd52c3191da1c12c0a8aeab5b
DaniaAbughoush <ms1dodo@gmail.com>
2021-04-27 20:29:17 +0300

    rewrite app and componanet file from start error happened

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,14 +1,14 @@
 import React from 'react';
- class Footer extends React.Component{
-     render(){
-         return(
-             <div>
-                 <footer>
+class Footer extends React.Component{
+  render(){
+    return(
+      <div>
+        <footer>
 
-                 <p>Dania Abughoush</p>
-                 </footer>
-             </div>
-         );
-     }
- }
- export default Footer;
+          <p>Dania Abughoush</p>
+        </footer>
+      </div>
+    );
+  }
+}
+export default Footer;
