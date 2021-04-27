d067927e12e292b896a24d9460df7cf6772f2de7
DaniaAbughoush <ms1dodo@gmail.com>
2021-04-27 15:50:51 +0300

    kk

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import React from 'react';
-import Header from './Components/header'
-import Footer from './Components/Footer'
-import Main from './Components/Main'
+import Header from './Components/header';
+import Footer from './Components/Footer';
+import Main from './Components/Main';
+import data from './Components/data.json';
+import SelectedBeast from './Components/SelectedBeast ';
+
+
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      SelectedBeast:{},
+      display:false,
+      data:data,
+
+    };
+  }
+
+  showModal = (selectedBeast) => {
+    // this.setState({ display: !this.state.display });
+    this.setState({ display: true, selectedBeast: selectedBeast });
+  }
+  
+  hideModal = () => {
+    this.setState({ display: false, selectedBeast: {} });
+  }
+
   render(){
     return(
-      <di>
+      <div>
         <Header/>
-        <Main/>
+        <Main
+          data={this.state.data}
+          // showModal={this.showModal}
+          // updateBeast={this.updateBeast}
+          handleClick={this.showModal}
+        />  
+        <SelectedBeast
+          display={this.state.display}
+          hideBeast={this.hideModal}
+          selectedBeast={this.state.selectedBeast}
+        />
         <Footer/>
-      </di>
-    )
+      </div>
+    );
   }
 }
+
 export default App;
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,30 +1,24 @@
 import React from 'react';
-import HornedBeasts from './HornedBeasts'
-import data from './data.json'
+import HornedBeasts from './HornedBeasts';
+import data from './data.json';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from 'react-bootstrap/Card';
+// import Card from 'react-bootstrap/Card';
 import CardColumns from 'react-bootstrap/CardColumns';
 
-
-
 class Main extends React.Component{
-    render(){
-      let hornedBeast=data.map(animal=>{
-        return <HornedBeasts title={animal.title} description={animal.description}  image_url = {animal.image_url} alt={animal.keyword}/>
-      });
-      
-       
+  render(){
+    let hornedBeast=data.map(animal=>{
+      return <HornedBeasts title={animal.title} description={animal.description} image_url = {animal.image_url} alt={animal.keyword}/>;
+    });
 
+    return(
+      <div>
+        <CardColumns>
 
-            
-return(
-  <div>
-     <CardColumns>
-
-    {hornedBeast}
-     </CardColumns>
-  </div>
-)
-    }
+          {hornedBeast}
+        </CardColumns>
+      </div>
+    );
+  }
 }
 export default Main;
